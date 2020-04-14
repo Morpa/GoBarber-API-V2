d5a61d6d037553288bd88fb2bb91ee7181b96782
Andre Morpa <andremorpa@gmail.com>
2020-04-14 19:15:03 +0100

    refactor(appointments): add repository by typeorm
    Add appointment and list

--- src/database/migrations/1586881634383-CreateAppointments.ts
+++ src/database/migrations/1586881634383-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1586881634383
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
