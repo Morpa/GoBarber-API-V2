61a1bf7b2a3b1d0f23420698d55b8a6091b39cae
Andre Morpa <andremorpa@gmail.com>
2020-04-15 17:10:16 +0100

    refactor(app): add error class

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -4,6 +4,8 @@ import { getCustomRepository } from 'typeorm';
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
+import AppError from '../errors/AppError';
+
 interface Request {
   provider_id: string;
   date: Date;
@@ -20,7 +22,7 @@ class CreateAppointmentService {
     );
 
     if (findAppointmentInSameDate) {
-      throw Error('This appointment is already bokked.');
+      throw new AppError('This appointment is already bokked.');
     }
 
     const appointment = appointmentsRepository.create({
