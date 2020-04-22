0d5a4475edb74baaa7503a2bc9d1efca48d4d520
Andre Morpa <andremorpa@gmail.com>
2020-04-22 13:37:29 +0100

    refactor(server): add cors

--- src/database/migrations/1586881634383-CreateAppointments.ts
+++ src/database/migrations/1586881634383-CreateAppointments.ts
@@ -3,6 +3,8 @@ import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 export default class CreateAppointments1586881634383
   implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
+
     await queryRunner.createTable(
       new Table({
         name: 'appointments',
