bb64b1e9f5a2b988f07b7c2d4a78d24e612716bd
Andre Morpa <andremorpa@gmail.com>
2020-05-15 09:47:52 +0100

    feat(storage): add amazon s3

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,21 +1,49 @@
-import path from 'path';
+import { resolve } from 'path';
 import crypto from 'crypto';
 import multer from 'multer';
 
-const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
+const tmpFolder = resolve(__dirname, '..', '..', 'tmp');
+
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+
+  tmpFolder: string;
+  uploadsFolder: string;
+
+  multer: {
+    storage: multer.StorageEngine;
+  };
+
+  config: {
+    disk: {};
+    aws: {
+      bucket: string;
+    };
+  };
+}
 
 export default {
+  driver: process.env.STORAGE_DRIVER,
+
   tmpFolder,
-  uploadsFolder: path.resolve(tmpFolder, 'uploads'),
+  uploadsFolder: resolve(tmpFolder, 'uploads'),
 
-  storage: multer.diskStorage({
-    destination: tmpFolder,
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('HEX');
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename(req, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('HEX');
+        const fileName = `${fileHash}-${file.originalname}`;
 
-      const fileName = `${fileHash}-${file.originalname}`;
+        return callback(null, fileName);
+      },
+    }),
+  },
 
-      return callback(null, fileName);
+  config: {
+    disk: {},
+    aws: {
+      bucket: 'morpa-app-gobarber',
     },
-  }),
-};
+  },
+} as IUploadConfig;
